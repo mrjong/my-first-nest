c57d7b8551828a8fc97dffb77a3dd11a8bd4c3e9
chenjiong <chen_jio@163.com>
2020-08-07 09:43:06 +0800

    认证用户

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from '../auth/local-auth.guard';
+import { AuthService } from '../auth/auth.service';
 @Controller()
 export class UsersController {
-  @UseGuards(AuthGuard('local'))
+  constructor(private readonly authService: AuthService) {}
+  @UseGuards(LocalAuthGuard)
   @Post('/auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
